263840ac0381f9e70b6a03461d2ad08d676f5c15
Pikrpan <54728861+Pikrpan@users.noreply.github.com>
2024-04-02 21:23:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var poleZvirat = ["pes", "kočka", "křeček"];
+
+var imgSrc = [
+    "https://cdn.alza.cz/Foto/ImgGalery/Image/border-kolie-beh.jpg",
+    "https://www.chlupaci.cz/user/articles/images/kocici-plemeno-kocka-domaci_(11).jpg",
+    "https://www.prokrecka.cz/wp-content/uploads/2021/02/krecek_syrsky_zlaty_1.jpg"
+];
+
+//Vytvoř proměnnou tak, aby se odkazovala na HTML element h2
+
+
+//Vytvoř proměnnou tak, aby se odkazovala na HTML element img
+
+
+//Vytvoř proměnnou, která bude sloužit k iteraci polí
+
+
+//Změň text v h2 elemntu na "Tohle je " + aktuální zvíře
+
+
+//Změň obrázek tak, aby souhlasil s textem h2 elementu (pomocí .setAtribute() příkazu)
+
+
+
+function dalsiZvire(){
+    //Přičti k proměnné určené k iteraci + 1
+
+
+    //Změň text v h2 elementu a obrázek podle proměnné určené k iteraci
+
+}
